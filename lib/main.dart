449b538107d300a6de490b4d72a68fb3cb9e738f
poseidon <61605433+poseidonKey@users.noreply.github.com>
2023-01-07 07:38:58 +0900

    initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-import 'screens/main_screens.dart';
-import 'theme.dart';
+void main() => runApp(const MyApp());
 
-void main() {
-  runApp(const CarrotMarketUI());
-}
-
-class CarrotMarketUI extends StatelessWidget {
-  const CarrotMarketUI({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'carrot_market_ui',
-      debugShowCheckedModeBanner: false,
-      home: const MainScreens(),
-      theme: theme(),
+      title: 'Material App',
+      home: MyHome(),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class MyHome extends StatelessWidget {
+  WebViewController? controller;
+  MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text("Code Factory"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                if (controller != null) {
+                  controller!.loadUrl("https://blog.codefactory.ai");
+                }
+              },
+              icon: const Icon(Icons.home)),
+          IconButton(
+            onPressed: () async {
+              if (controller != null && await controller!.canGoBack()) {
+                controller!.goBack();
+              }
+            },
+            icon: const Icon(Icons.arrow_left_sharp),
+          )
+        ],
+      ),
+      body: WebView(
+        onWebViewCreated: (controller) {
+          this.controller = controller;
+        },
+        initialUrl: "https://blog.codefactory.ai",
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
   }
 }
